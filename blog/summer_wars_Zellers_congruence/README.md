3c1507a08ee840ce9b4f8228c7f35e620eaafaee
nwiizo <syu.m.5151@gmail.com>
2017-01-03 03:19:59 +0900

    201701030319

---
+++ blog/summer_wars_Zellers_congruence/README.md
@@ -0,0 +1,1 @@
+[夏を感じます。【pythonによるツェラーの公式の実装】](http://syu-m-5151.hatenablog.com/entry/2017/01/03/030946)
---
+++ blog/summer_wars_Zellers_congruence/Zellers_congruence.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    print("先輩の誕生日いつですか?")
+    year = int(input("年は?:"))
+    month = int(input("月は?:"))
+    day = int(input("何日?:"))
+    C = year/100
+    Y = year%100
+    F = int(day) + (26*(int(month)+1)/10) + int(Y) + (int(Y)/4) + 5*C + (C/4)
+    X = (F % 7) + 1
+    week = ["土","日","月","火","水","木","金"]
+    print(week[int(X)]+"曜日です",)
+    print(str(year) +"年"+str(month) + "月"+ str(day) +"日は"+str(week[int(X)])+"曜日でした")
+    print("ひょっとして全部覚えてるの??")
+    print("いえ、モジュロ演算というのを使って…当たってました?")
+    print("ごめん、何曜日か知らないや!!")
