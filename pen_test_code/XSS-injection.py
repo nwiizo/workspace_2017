422b559b07f179e93423530155a86bc7765142c8
Shuya Motouchi <syu.m.5151@gmail.com>
2017-01-11 15:17:05 +0900

    Update XSS-injection.py

--- pen_test_code/XSS-injection.py
+++ pen_test_code/XSS-injection.py
@@ -12,11 +12,11 @@ with open('XSS-vectors.txt') as f:
 	content = res.read()
 	#  check the attack vector is printed in the response.
     	if content.find(line) > 0:
-    		print "Possible XXS"
+    		print("Possible XXS")
     	
 	output = open('response/'+str(attackNumber)+'.txt', 'w')
 	output.write(content)
 	output.close()
-	print attackNumber
+	print(attackNumber)
 	attackNumber += 1
 	
