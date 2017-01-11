246488c7a0fcf112f1f11f259a6170a066c229f1
Shuya Motouchi <syu.m.5151@gmail.com>
2017-01-11 15:03:08 +0900

    Update Crypto_AES.py

--- pen_test_code/Crypto_AES.py
+++ pen_test_code/Crypto_AES.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from Crypto.Cipher import AES
 encrypt_AES = AES.new('secret-key-12345', AES.MODE_CBC, 'This is an IV456')
 message = "This is message "
 ciphertext = encrypt_AES.encrypt(message)
-print ciphertext
+print(ciphertext)
 decrypt_AES = AES.new('secret-key-12345', AES.MODE_CBC, 'This is an IV456')
 message_decrypted =  decrypt_AES.decrypt(ciphertext)
-print message_decrypted
+print(message_decrypted)
