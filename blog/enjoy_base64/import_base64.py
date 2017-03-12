73678e71bd378ef81eeaa0601e0e985389880ff3
nwiizo <syu.m.5151@gmail.com>
2017-03-13 08:47:56 +0900

    it seems to work fine.

--- blog/enjoy_base64/import_base64.py
+++ blog/enjoy_base64/import_base64.py
@@ -2,15 +2,20 @@
 # -*- coding:utf-8 -*-
 
 import base64
+import fire
 
-data = "motouchi"
-
-print(("data:{}").format(data))
+class base64_test(object):
+    def base(self,data):
+        print(("data:{}").format(data))
 # base 64でencode
-enc_data = base64.encodestring(data.encode("utf8")).decode("ascii")
+        enc_data = base64.encodestring(data.encode("utf8")).decode("ascii")
 # そんなデータを出力
-print(("encode data:{}").format(enc_data))
+        print(("encode data:{}").format(enc_data))
 # decodeするよ
-dec_data = base64.decodestring(enc_data.encode("ascii")).decode("utf8")
+        dec_data = base64.decodestring(enc_data.encode("ascii")).decode("utf8")
 # そんなデータを出力
-print (("decode data:{}").format(dec_data))
+        print (("decode data:{}").format(dec_data))
+
+
+if __name__ == '__main__':
+    fire.Fire(base64_test)
