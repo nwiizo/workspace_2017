ae33728f91f17dcff9c06f7ef59d7e42e4406426
Shuya Motouchi <syu.m.5151@gmail.com>
2017-12-12 22:28:13 +0900

    Update main.rs

--- Rust/example/libpnet/packetdump/src/main.rs
+++ Rust/example/libpnet/packetdump/src/main.rs
@@ -1,13 +1,3 @@
-// Copyright (c) 2014, 2015 Robert Clipsham <robert@octarineparrot.com>
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
-/// This example shows a basic packet logger using libpnet
-
 extern crate pnet;
 
 
