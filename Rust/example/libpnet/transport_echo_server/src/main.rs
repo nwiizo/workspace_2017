e90e502d9cb573c5346bf8e003c2cb4e26257b98
Shuya Motouchi <syu.m.5151@gmail.com>
2017-12-12 22:29:27 +0900

    Update main.rs

--- Rust/example/libpnet/transport_echo_server/src/main.rs
+++ Rust/example/libpnet/transport_echo_server/src/main.rs
@@ -1,14 +1,4 @@
-// Copyright (c) 2014, 2015 Robert Clipsham <robert@octarineparrot.com>
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 extern crate pnet;
-
-/// A simple echo server for packets using a test protocol
 
 use std::iter::repeat;
 
